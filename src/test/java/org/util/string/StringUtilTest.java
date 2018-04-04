89abc54b937c5f3982db2cea115c481a661c86f8
VLysak <vitalii.v.lysak@gmail.com>
2018-04-04 21:39:21 +0300

    added junit dependency, added dummy test

---
+++ src/test/java/org/util/string/StringUtilTest.java
@@ -0,0 +1,11 @@
+package org.util.string;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+
+    @Test
+    public void testDoNothing() {
+        System.out.println("This is a dummy test");
+    }
+}
