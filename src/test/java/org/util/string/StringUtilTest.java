8a9eaeffec34a3469549361ceccaa2540de97e00
VLysak <vitalii.v.lysak@gmail.com>
2018-04-13 19:19:46 +0300

    added1

--- src/test/java/org/util/string/StringUtilTest.java
+++ src/test/java/org/util/string/StringUtilTest.java
@@ -1,5 +1,6 @@
 package org.util.string;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class StringUtilTest {
@@ -7,6 +8,11 @@ public class StringUtilTest {
     @Test
     public void testDoNothing() {
         System.out.println("This is a dummy test");
+    }
+
+    @Test
+    public void testDoNothingSupposedToFail() {
+        Assert.fail();
     }
 
 }
